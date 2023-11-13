15e9798b5b524c7d9aa0cab1fcc7f956f5822500
beastrobel <beatrizstrobel@proton.me>
2023-11-13 16:26:06 -0500

    Added .gitignore, installed npms, added handlebars to server.js. Server is running but not rendering yet

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const path = require('path');
-const api = require('./routes/post.js');
+const api = require('./api/post.js');
 const PORT = process.env.PORT || 3001;
 
+//Homepage middleware
+app.use(express.static('public'));
+
+// Add express-session and store as Express.js middleware
+//app.use(session(sess));
+const hbs = exphbs.create({ });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}.`);
-)
+    console.log(`Server running on port ${PORT}.`)
+);
