9659e10132de76ed1ef884db2d4b14e2a39ef67e
beastrobel <beatrizstrobel@proton.me>
2023-12-29 10:17:20 -0500

    Edited server.js, added session

--- server.js
+++ server.js
@@ -3,21 +3,40 @@ const app = express();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const api = require('./api/post.js');
+const routes = require('./controllers');
 const PORT = process.env.PORT || 3001;
 
-//Homepage middleware
-app.use(express.static('public'));
-
-// Add express-session and store as Express.js middleware
-//app.use(session(sess));
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({ });
+
+//Session
+const sess = {
+    secret: 'Cookie Monster',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+app.use(session(sess));
+
+//Handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}.`)
