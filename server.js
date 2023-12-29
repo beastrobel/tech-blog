fe170fb48af4ed2c89f033d89f05d5c8eafe87cc
beastrobel <beatrizstrobel@proton.me>
2023-12-29 12:01:42 -0500

    Tech blog is displaying properly, working on authentication bug and css

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const path = require('path');
 const routes = require('./controllers');
-const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({ });
 
-//Session
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
-    secret: 'Cookie Monster',
-    cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use(session(sess));
 
-//Handlebars
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -38,6 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}.`)
-);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
